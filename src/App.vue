273a103ff8c82b6e5f4dd909d511bb7bccb6cb28
Mario Fink <mario.fink@goodyear.com>
2023-11-28 20:47:25 +0100

    add Tailwind CSS for basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,18 @@
 <!-- App.vue -->
 <template>
-  <div id="app">
-    <PokemonViewer />
-  </div>
+  <PokemonViewer />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import PokemonViewer from './components/PokemonViewer.vue'
+import { defineComponent } from 'vue';
+import PokemonViewer from './components/PokemonViewer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     PokemonViewer
   }
-})
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-button {
-  margin-top: 20px;
-  padding: 10px;
-  cursor: pointer;
-}
-</style>
+<style></style>
--- src/components/PokemonViewer.vue
+++ src/components/PokemonViewer.vue
@@ -1,13 +1,21 @@
 <!-- PokemonViewer.vue -->
 <template>
-  <div>
-    <h1>Random Pokémon Viewer</h1>
-    <div v-if="loading">Loading...</div>
-    <div v-else>
-      <img :src="pokemonSprite" :alt="pokemon.name" />
-      <h2>{{ pokemon.name }}</h2>
+  <div class="flex flex-col items-center mt-8">
+    <h1 class="text-2xl font-bold">Random Pokémon Viewer</h1>
+    <div class="mt-4 mb-8 h-[120px]">
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <img :src="pokemonSprite" :alt="pokemon.name" />
+        <h2 class="text-center">{{ pokemon.name }}</h2>
+      </div>
     </div>
-    <button @click="fetchRandomPokemon">Get Random Pokémon</button>
+    <button
+      type="button"
+      @click="fetchRandomPokemon"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    >
+      Get Random Pokémon
+    </button>
   </div>
 </template>
 
