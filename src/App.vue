7342470cb1179ed418c3d69273132e850357ae4e
Mario Fink <mariofink@users.noreply.github.com>
2024-09-16 15:46:43 +0200

    add vue-query-devtools

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <!-- App.vue -->
 <template>
   <PokemonViewer />
+  <VueQueryDevtools />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 import PokemonViewer from './components/PokemonViewer.vue';
+
 
 export default defineComponent({
   name: 'App',
   components: {
-    PokemonViewer
+    PokemonViewer,
+    VueQueryDevtools
   }
 });
 </script>
