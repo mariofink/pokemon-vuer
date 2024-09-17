b57f1f48adbcf7ce53ca8fd60ac0c9fa8e4a89e4
Mario Fink <mariofink@users.noreply.github.com>
2024-09-17 11:12:44 +0200

    fix static querykey

--- src/queries/pokemonQuery.ts
+++ src/queries/pokemonQuery.ts
@@ -1,14 +1,14 @@
 import { PokeApiService, type Pokemon } from '@/services/PokeApiService';
 import { useQuery } from '@tanstack/vue-query';
-import { watch, type Ref } from 'vue';
+import { computed, watch, type Ref } from 'vue';
 
 const pokeApiService = new PokeApiService();
 
 export function usePokemonQuery(id: Ref<number>) {
-  const queryKey = () => ['pokemon', id.value];
+  const queryKey = computed(() => ['pokemon', id.value]);
 
   const query = useQuery({
-    queryKey: queryKey(),
+    queryKey: queryKey,
     queryFn: (): Promise<Pokemon> => pokeApiService.getPokemonById(id.value)
   });
 
